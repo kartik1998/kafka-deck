55a22955a03988ddfb7426356e60667bd1ae159e
kartik1998 <kartikrawat61@gmail.com>
2021-10-25 23:06:19 +0530

    Add: js-reference

---
+++ js-reference/README.md
@@ -0,0 +1,10 @@
+## Kafka Node Reference
+
+- To create the users topic first run: `node topic.js`
+  - Sample output: 
+
+```
+[2021-10-25T17:36:07.561Z] connecting...
+[2021-10-25T17:36:07.606Z] connected!..
+[2021-10-25T17:36:07.635Z] "Users" partition created
+```
---
+++ js-reference/topic.js
@@ -0,0 +1,40 @@
+const { Kafka } = require("kafkajs");
+
+global.log = (message) => {
+  console.log(`[${new Date().toISOString()}] ${message}`);
+};
+
+async function run() {
+  try {
+    /* create an admin tcp connection */
+    const kafka = new Kafka({
+      clientId: "myapp",
+      brokers: ["localhost:29092"],
+    });
+
+    const admin = kafka.admin();
+    log("connecting...");
+    await admin.connect();
+    log("connected!..");
+
+    /**
+     * create "Users" topic with 2 partitions [A - M, N - Z]
+     */
+    await admin.createTopics({
+      topics: [
+        {
+          topic: "Users",
+          numPartitions: 2,
+        },
+      ],
+    });
+    log('"Users" partition created');
+    await admin.disconnect();
+  } catch (err) {
+    console.error(err);
+  } finally {
+    process.exit(0);
+  }
+}
+
+run();
